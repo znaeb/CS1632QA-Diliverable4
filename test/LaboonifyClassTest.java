3da03231704c84fb419c548dc33155710a39ef66
znaeb <urnotbeanz@gmail.com>
2016-07-06 01:29:05 -0400

    two tests done

--- test/LaboonifyClassTest.java
+++ test/LaboonifyClassTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.Random;
 
 /**
  *
@@ -38,17 +39,68 @@ public class LaboonifyClassTest {
 
     /**
      * Test of Laboonify method, of class LaboonifyClass.
+     * tests whether the laboonified array has one more element than the input array
      */
     @Test
-    public void testLaboonify() {
+    public void testLaboonifySize() {
         System.out.println("Laboonify");
-        int[] x = null;
+        int[][] testHolder=new int[100][];
+        for(int i=0;i<testHolder.length;i++){
+            Random rand=new Random();
+            int arraySize=rand.nextInt(100)+1;
+            int[] holderArray=new int[arraySize];
+            for(int j=0;j<arraySize;j++){
+                holderArray[j]=(rand.nextInt(100)+1);
+            }
+            testHolder[i]=holderArray;
+        }
+        //int[] x = null;
         LaboonifyClass instance = new LaboonifyClass();
-        int[] expResult = null;
-        int[] result = instance.Laboonify(x);
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        for (int i=0;i<testHolder.length;i++){
+            int expResult = (testHolder[i].length)+1;
+            int result = instance.Laboonify(testHolder[i]).length;
+            assertEquals(expResult, result);    
+        }
     }
+    /**
+     * Test of Laboonify method, of class LaboonifyClass.
+     * tests whether the laboonified array has all the values of the original array squared
+     */
+    @Test
+    public void testLaboonifySquare() {
+        System.out.println("Laboonify");
+        int[][] testHolder=new int[100][];
+        for(int i=0;i<testHolder.length;i++){
+            Random rand=new Random();
+            int arraySize=rand.nextInt(100)+1;
+            int[] holderArray=new int[arraySize];
+            for(int j=0;j<arraySize;j++){
+                holderArray[j]=(rand.nextInt(100)+1);
+            }
+            testHolder[i]=holderArray;
+        }
+        //int[] x = null;
+        LaboonifyClass instance = new LaboonifyClass();
+        for (int i=0;i<testHolder.length;i++){
+            for (int j=0;j<testHolder[i].length;j++){
+                int expResult = ((testHolder[i])[j])*((testHolder[i])[j]);
+                int result = (instance.Laboonify(testHolder[i]))[j];
+                assertEquals(expResult, result);
+            }
+        }
+    }
+    
+    
+    /*
+    @Property(trials = 100)
+    public void testAddition(int number) {
+
+        System.out.println("Generated number for testAddition: " + number);
+
+        Calculator calculator = new Calculator();
+        calculator.add(number);
+        assertEquals(calculator.getResult(), number);
+    }
+    */
     
 }
